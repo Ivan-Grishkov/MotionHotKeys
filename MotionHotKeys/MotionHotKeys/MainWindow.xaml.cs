44b456c002ed6e1eb3c7622c3d79f0e0547aaa13
Ivan <grishkov.ivan@gmail.com>
2013-05-18 16:12:46 +0300

    try to raise event

---
+++ MotionHotKeys/MotionHotKeys/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MotionHotKeys.RaiseEvents;
+
+namespace MotionHotKeys
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _textStr;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void RaiseSpace_Click(object sender, RoutedEventArgs e)
+        {
+            EventsGenerator.RiseSpacePressed();
+        }
+
+        public string TextStr
+        {
+            get { return _textStr; }
+            set
+            {
+                //_textStr = value;
+                //OnPropertyChanged("TextStr");
+                EventsGenerator.RiseSpacePressed();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MotionHotKeys/MotionHotKeys/RaiseEvents/EventsGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace MotionHotKeys.RaiseEvents
+{
+    public class EventsGenerator
+    {
+        public static void RiseSpacePressed()
+        {
+            var key = Key.Space;                    // Key to send
+            var target = Keyboard.FocusedElement;    // Target element
+            var routedEvent = Keyboard.KeyDownEvent; // Event to send
+            //Thread.Sleep(1000);
+            
+
+            target.RaiseEvent(
+              new KeyEventArgs(
+                Keyboard.PrimaryDevice,
+                PresentationSource.FromVisual(Keyboard.PrimaryDevice.FocusedElement as Visual),
+                0,
+                key) { RoutedEvent = routedEvent }
+            );
+
+            key = Key.A; 
+            target.RaiseEvent(
+              new KeyEventArgs(
+                Keyboard.PrimaryDevice,
+                PresentationSource.FromVisual(App.Current.MainWindow),
+                0,
+                key) { RoutedEvent = routedEvent }
+            );
+            target.RaiseEvent(
+              new KeyEventArgs(
+                Keyboard.PrimaryDevice,
+                PresentationSource.FromVisual(App.Current.MainWindow),
+                0,
+                key) { RoutedEvent = routedEvent }
+            );
+            target.RaiseEvent(
+              new KeyEventArgs(
+                Keyboard.PrimaryDevice,
+                PresentationSource.FromVisual(App.Current.MainWindow),
+                0,
+                key) { RoutedEvent = routedEvent }
+            );
+        }
+
+
+    }
+}
